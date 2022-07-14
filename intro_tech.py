1d4d6bf5b977b91b9e0d2060804d66b96a941fbe
Usman Musa <usmanmusa1920@gmail.com>
2022-07-14 06:59:11 +0100

    first commit

---
+++ intro_tech.py
@@ -0,0 +1,15 @@
+"""
+
+    INTRODUCTION TO TECH STACK
+
+    In this content, we are going to be breaking down how to use some tech stack, of which will make your project more productive, or make you more valuable to a company, let us know something little about it.
+    
+    Tech stack is the main focus in every IT field whether software engineering, web development, network engineering, etc. Tech stack include programming language that a company is using or you in person you are using, frameworks that make your project faster to be build, other tools that you use to manage and simplify the time consuming when working on a project, like a text editor (VS-code) which come with an auto-completion when writing a programm, instead of writing your code using something like notepad which is static.
+    
+    Other tech stack like version control system, SVN, etc which make it easy when contributing to an open source project or working with a team remotely, there are tons of tech stack that make our life easy now-adays when working to a company, startup or even personal project.
+    
+        git clone https://github.com/usmanmusa1920/repository.git
+        git push heroku master
+        vs-code auto completion
+
+"""
